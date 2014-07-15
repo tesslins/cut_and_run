b6e2b52cc7d7e69535fa14523f5d0bf9d2f6554e
Tess Bakke <tesslins@salvaje.local>
2014-07-14 17:38:36 -0700

    debugging ajax and jquery

--- routes.py
+++ routes.py
@@ -14,11 +14,9 @@ mmf = MapMyFitness(api_key='hhp3ye7mq97jnuz8rxfzwc3fz39ef3rp',
 
 @app.route('/')
 def homepage():
-    lat_lng = (37.8,-122.2) #tuple
-    #lat_lng=json.dumps(lat_lng) #list (the initialize_map.js seems to require a tuple!)
-    return render_template("index.html", lat_lng=lat_lng)
+    return render_template("index.html")
 
-@app.route('/route')
+@app.route('/_route')
 def search_route():
     '''Search for all routes near latitute-longitude, maximum and minimum distance are optional. Distances currently in meters and  default to minimum of 1 mile (1609 meters) and 10 miles (16093 meters). '''
     lat_lng = lat_lng #add check here - request lat_long to continue if none is entered
@@ -28,22 +26,26 @@ def search_route():
                                         minimum_distance=min_distance,
                                         maximum_distance=max_distance)
     page_range = routes_paginator.page_range
-    route_list = []
-    the_page = routes_paginator.page(page_range[0])
-    for route in the_page:
-        route_list.append(route)
-    return route_list
+    if len(page_range) > 0:
+        route_list = []
+        the_page = routes_paginator.page(page_range[0])
+        for route in the_page:
+            route_list.append(route)
+        return render_route(route_list) #add check here (if length of route_list = 0, maybe if length of route_list < 5, hollaback with a message)
+    else: #add check here (if length of page_range = 0, tell the user something? the "close" in close_to_location is not specified)
+        print "Warning! Page_range length is: " , page_range
 
 def render_route(route_list):
     '''Search for a route by ID, get points as lat/lng tuples, turn into a geoJSON of lat/lng lists.'''
     i = 0
     for route[i] in route_list:
         route = mmf.route.find(route.id)
-        route_points = route.points(geojson=True) #this only ALMOST creates a geoJSON, requires the next 3 lines
+        route_points = route.points(geojson=True) #this ALMOST creates a geoJSON, requires the next 3 lines
         lat_lng_tuples = route_points['coordinates']
         lat_lng_lists = [list(point) for point in lat_lng_tuples]
         route_points['coordinates'] = lat_lng_lists
         return route_points
+        #i += 1 #need to figure looping through route in response to click on "no" on map page
         
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
--- static/js/initialize_map.js
+++ static/js/initialize_map.js
@@ -1,6 +1,3 @@
-//declare namespace
-var runAround = {};
-
 // Declare the map & geocoder and always console log.
 var map;
 var geocoder;
@@ -14,7 +11,7 @@ function trace(message)
 }
 
 // Create the map.
-runAround.initialize = function() {
+function initialize () {
   var latlng = new google.maps.LatLng(37.8,-122.2);
   var mapOptions = {
     zoom: 12,
@@ -24,16 +21,18 @@ runAround.initialize = function() {
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
-// Geocode search box for zip code.
+// Geocode centering on zip code.
+// Need to add a check to ensure that this is a real address - 94616 sent me to eastern Europe?
 geocoder = new google.maps.Geocoder();
-runAround.geocode = function() 
-{
+
+function geocode () {
   var address = $('#address').val();
   geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) 
       {
-          map.setCenter(results[0].geometry.location);
-          // This places a marker in the center, don't want it.
+          var lat_lng = results[0].geometry.location
+          map.setCenter(lat_lng);
+          // This places a marker in the center, which I do not want.
           //var marker = new google.maps.Marker({
           //    map: map, 
           //    position: results[0].geometry.location
@@ -41,7 +40,31 @@ runAround.geocode = function()
       } 
       else 
       {
-          alert("Geocode was not successful for the following reason: " + status);
+        alert("Geocode was not successful for the following reason: " + status);
       }
+      return lat_lng;
   });
+  
 }
+
+  // Rendering route.
+  function submitdata (lat_lng) {
+      $.getJSON($SCRIPT_ROOT + '/_route', {
+        lat_lng: lat_lng,
+        min_distance: $('input[name="min_distance"]').val(),
+        max_distance: $('input[name="max_distance"]').val()
+    }, function(data) {
+      $("#result").text(data.result);
+    return false;
+    });  
+  };
+  
+  $(document).ready(function() {
+    initialize();
+    
+    $('input#render').bind('click', function () {
+      var lat_lng = geocode();
+      submitdata(lat_lng);
+    });
+  });
+
